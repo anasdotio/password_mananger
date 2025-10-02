8d9d61d044da82d8bbd9cb49f75fbc2766f4ddd5
Anas Khan <anasroyal385@gmail.com>
2025-10-02 21:54:27 +0530

    setup the server and install express or mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
 const app = require("./src/app");
+
+const PORT = process.env.PORT || 3000;
+
+app
+  .listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+  })
+  .on("error", (err) =>
+    console.log(`Error occur while start the server : ${err.message}`)
+  );
