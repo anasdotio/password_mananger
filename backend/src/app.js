eb3bd0ace2a7b23f94732c30a323880bc50689bf
Anas Khan <anasroyal385@gmail.com>
2025-10-02 22:05:30 +0530

    add authentication routes and setup express middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const authRoutes = require("./routes/auth.routes");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use("/api/auth", authRoutes);
+
+app.get("/check", (req, res) => {
+  res.status(200).json({ message: "server work well !" });
+});
 
 module.exports = app;
---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.route("/register").post(() => {});
+router.route("/login").post(() => {});
+
+module.exports = router;
